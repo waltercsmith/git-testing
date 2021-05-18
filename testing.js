f4c1ebdcd086a62b58674f01ecb2e098251501c5
Walter Smith <walter.c.smith01@gmail.com>
2021-05-18 15:00:09 -0500

    Created a basic js function that holds three parameters that returns the sum of the three

--- testing.js
+++ testing.js
@@ -1,1 +1,13 @@
 var hello = "hello";
+
+
+/*This code below is apart of the branch called second_branch
+* I'm just reviewing the concept of branches/keeping up with some steady practice of the basics
+*
+* */
+
+function addThree(a,b,c){
+    return a + b + c;
+}
+
+console.log(addThree(1,2,3));
